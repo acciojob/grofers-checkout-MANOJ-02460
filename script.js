f982bab4ebcfe76cbbfdc386ccccb387097d2c25
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-07-06 08:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+let pricesElement = document.querySelectorAll(".price");
+let totalSum = 0;
+
+pricesElement.forEach((element)=>{
+	const priceText = element.textContent.trim();
+	const priceValue = Number(priceText)||0;
+
+	totalSum += priceValue;
+});
+
+let table = document.querySelector("table");
+let row = document.createElement("tr");
+let newCell = document.createElement("td");
+
+newCell.textContent = `Total: ${totalSum}`;
+newCell.setAttribute("id","ans");
+
+
+row.appendChild(newCell);
+table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
